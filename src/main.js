33e1d1a7ddb1aa6f50d0f3f89c646083eb4763fd
Mikhail <plohov.mm@mail.ru>
2020-08-27 01:50:48 +0500

    Учебный проект. Раздел 3. Шаблонизируй это

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
 import {createSiteMenuTemplate} from "./view/site-menu.js";
-import {createSiteFilterTemplate} from "./view/site-filter.js";
+import {createSiteFilterTemplate} from "./view/site-filter/site-filter.js";
 import {createSiteBoardTemplate} from "./view/board.js";
 import {createBoardSortTemplate} from "./view/board-sort.js";
 import {createTasksListTemplate} from "./view/tast-list.js";
 import {createTaskTemplate} from "./view/task.js";
-import {createTaskEditTemplate} from "./view/task-edit.js";
+import {createTaskEditTemplate} from "./view/task-edit/task-edit.js";
 import {createLoadMoreButtonTemplate} from "./view/load-more.js";
 
-const TASK_COUNT = 12;
+import {generateTask} from "./mock/task.js";
+import {generateFilter} from "./mock/filter.js";
+
+const TASK_COUNT = 36;
+const TASK_COUNT_PER_STEP = 8;
+
+const tasks = new Array(TASK_COUNT).fill().map(generateTask);
+const filters = generateFilter(tasks);
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -18,7 +25,7 @@ const siteMainControlElement = document.querySelector(`.main__control`);
 
 render(siteMainControlElement, createSiteMenuTemplate(), `beforeend`);
 
-render(siteMainElement, createSiteFilterTemplate(), `beforeend`);
+render(siteMainElement, createSiteFilterTemplate(filters), `beforeend`);
 
 render(siteMainElement, createSiteBoardTemplate(), `beforeend`);
 
@@ -26,12 +33,35 @@ const siteBoardElement = document.querySelector(`.board`);
 
 render(siteBoardElement, createBoardSortTemplate(), `beforeend`);
 render(siteBoardElement, createTasksListTemplate(), `beforeend`);
-render(siteBoardElement, createLoadMoreButtonTemplate(), `beforeend`);
 
 const siteTasksListElement = document.querySelector(`.board__tasks`);
 
-render(siteTasksListElement, createTaskEditTemplate(), `beforeend`);
+render(siteTasksListElement, createTaskEditTemplate(tasks[0]), `beforeend`);
 
-for (let i = 1; i <= TASK_COUNT; i++) {
-  render(siteTasksListElement, createTaskTemplate(), `beforeend`);
+for (let i = 1; i < Math.min(tasks.length, TASK_COUNT_PER_STEP); i++) {
+  render(siteTasksListElement, createTaskTemplate(tasks[i]), `beforeend`);
 }
+
+if (tasks.length > TASK_COUNT_PER_STEP) {
+  let renderedTaskCount = TASK_COUNT_PER_STEP;
+
+  render(siteBoardElement, createLoadMoreButtonTemplate(), `beforeend`);
+
+  const loadMoreButton = siteBoardElement.querySelector(`.load-more`);
+
+  loadMoreButton.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+
+    tasks
+    .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
+    .forEach((task) => render(siteTasksListElement, createTaskTemplate(task), `beforeend`));
+
+    renderedTaskCount += TASK_COUNT_PER_STEP;
+
+    if (renderedTaskCount >= tasks.length) {
+      loadMoreButton.remove();
+    }
+  });
+}
+
+
---
+++ src/view/task-edit/task-edit.js
@@ -0,0 +1,78 @@
+import {isTaskExpired, isTaskRepeating} from "../../utils.js";
+import {createTaskEditDateTemplate} from "./date.js";
+import {createTaskEditRepeatingTemplate} from "./repeating.js";
+import {createTaskEditColorsTemplate} from "./colors.js";
+
+export const createTaskEditTemplate = (task = {}) => {
+  const {
+    color = `black`,
+    description = ``,
+    dueDate = null,
+    repeating = {
+      mo: false,
+      tu: false,
+      we: false,
+      th: false,
+      fr: false,
+      sa: false,
+      su: false
+    }
+  } = task;
+
+  const deadlineClassName = isTaskExpired(dueDate)
+    ? `card--deadline`
+    : ``;
+
+  const repeatingClassName = isTaskRepeating(repeating)
+    ? `card--repeat`
+    : ``;
+
+  const dateTemplate = createTaskEditDateTemplate(dueDate);
+  const repeatingTemplate = createTaskEditRepeatingTemplate(repeating);
+  const colorsTemplate = createTaskEditColorsTemplate(color);
+
+  return (
+    `<article class="card card--edit card--${color} ${deadlineClassName} ${repeatingClassName}">
+      <form class="card__form" method="get">
+        <div class="card__inner">
+          <div class="card__color-bar">
+            <svg class="card__color-bar-wave" width="100%" height="10">
+              <use xlink:href="#wave"></use>
+            </svg>
+          </div>
+
+          <div class="card__textarea-wrap">
+            <label>
+              <textarea
+                class="card__text"
+                placeholder="Start typing your text here..."
+                name="text"
+              >${description}</textarea>
+            </label>
+          </div>
+
+          <div class="card__settings">
+            <div class="card__details">
+              <div class="card__dates">
+                ${dateTemplate}
+                ${repeatingTemplate}
+              </div>
+            </div>
+
+            <div class="card__colors-inner">
+              <h3 class="card__colors-title">Color</h3>
+              <div class="card__colors-wrap">
+                ${colorsTemplate}
+              </div>
+            </div>
+          </div>
+
+          <div class="card__status-btns">
+            <button class="card__save" type="submit">save</button>
+            <button class="card__delete" type="button">delete</button>
+          </div>
+        </div>
+      </form>
+    </article>`
+  );
+};
